3540a27803f5850b5e1fed10a9707ae4b2759b28
Zayooo00 <mrcold135@gmail.com>
2023-11-02 00:20:03 +0100

    created firebase error codes file

---
+++ firebase/firebaseErrorCodes.ts
@@ -0,0 +1,6 @@
+export const ERROR_INVALID_EMAIL = 'auth/invalid-email';
+export const ERROR_WRONG_PASSWORD = 'auth/wrong-password';
+export const ERROR_USER_NOT_FOUND = 'auth/user-not-found';
+export const ERROR_MISSING_PASSWORD = 'auth/missing-password';
+export const ERROR_POPUP_CLOSED = 'auth/popup-closed-by-user';
+export const ERROR_NETWORK_REQUEST_FAILED = 'network-request-failed';
