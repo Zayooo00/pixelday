d2516ae5c91d3072345899e5265d9fd99956b4c0
Zayooo00 <mrcold135@gmail.com>
2023-11-02 00:22:11 +0100

    implemented sign up functionality

--- app/(pages)/(auth)/sign-up/page.tsx
+++ app/(pages)/(auth)/sign-up/page.tsx
@@ -1,6 +1,39 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { SignUpSchema } from "@/helpers/signUpValidator";
+import { auth, createUserWithEmailAndPassword } from "@/firebase/firebase";
+import Spinner from "@/components/spinner";
 
 export default function SignUp() {
+  const [user, setUser] = useState({ email: "", username: "", password: "" });
+  const [error, setError] = useState("");
+  const [isAuthLoading, setIsAuthLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsAuthLoading(true);
+    try {
+      SignUpSchema.parse(user);
+
+      await createUserWithEmailAndPassword(auth, user.email, user.password);
+
+      router.push("/dashboard");
+    } catch (error: unknown) {
+      if (error instanceof Error && "errors" in error) {
+        setError((error as any).errors[0].message);
+      } else if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setIsAuthLoading(false);
+    }
+  };
+
   return (
     <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
       <Link href={"/"} className="-mt-16 sm:-mt-52">
@@ -9,6 +42,11 @@ export default function SignUp() {
         </h1>
       </Link>
       <div className="flex flex-col items-center mt-16">
+        {error && (
+          <p className="text-xl rounded-sm -mt-10 mb-3 px-2 bg-white text-red-500">
+            {error}
+          </p>
+        )}
         <label className="flex items-center mb-4">
           <span className="w-32 text-left text-2xl text-white">Email:</span>
           <input
@@ -16,6 +54,7 @@ export default function SignUp() {
             type="text"
             name="name"
             placeholder="Enter email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
           />
         </label>
         <label className="flex items-center mb-4">
@@ -25,6 +64,7 @@ export default function SignUp() {
             type="text"
             name="name"
             placeholder="Enter username"
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
           />
         </label>
         <label className="flex items-center">
@@ -33,10 +73,14 @@ export default function SignUp() {
             type="password"
             className="px-2 py-1 w-36 sm:w-48 text-xl border-solid border-2 border-yellow-600 focus:border-yellow-700 focus:outline-none"
             placeholder="********"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
         </label>
-        <button className="px-4 py-1 mt-6 text-xl text-yellow-700 bg-white rounded-md border-solid border-4 border-yellow-700 transform transition-transform duration-500 hover:scale-110 relative group">
-          Sign Up
+        <button
+          className="px-4 py-1 mt-6 text-xl text-yellow-700 bg-white rounded-md border-solid border-4 border-yellow-700 transform transition-transform duration-500 hover:scale-110 relative group"
+          onClick={handleSignUp}
+        >
+          {isAuthLoading ? <Spinner/> : "Sign Up"}
           <span className="absolute bottom-[-0.5rem] left-2 w-full h-2 transform scale-y-0 bg-yellow-700 transition-transform duration-500 group-hover:scale-y-100 rounded-l-sm"></span>
           <span className="absolute top-2 right-[-0.5rem] h-full w-2 transform scale-x-0 bg-yellow-700 transition-transform duration-500 group-hover:scale-x-100 rounded-r-sm"></span>
         </button>
