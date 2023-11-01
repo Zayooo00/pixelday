c2a42f1e933515927648da5414af4d07c4b68119
Zayooo <mrcold135@gmail.com>
2023-11-02 00:18:16 +0100

    Create sign in page #10 #11 (#17)
    * global.css cleanup
    
    * updated layout file to display background image on every page
    
    * moved background image component to layout file
    
    * renamed background image assets
    
    * created initial sign in page
    
    * created initial sign up page
    
    * mobile viewport tailwind fix
    
    * mobile ui height adjustment

---
+++ app/(pages)/(auth)/sign-up/page.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+export default function SignUp() {
+  return (
+    <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+      <Link href={"/"} className="-mt-16 sm:-mt-52">
+        <h1 className="text-6xl sm:text-8xl font-bold text-white text-stroke">
+          Pixel Day
+        </h1>
+      </Link>
+      <div className="flex flex-col items-center mt-16">
+        <label className="flex items-center mb-4">
+          <span className="w-32 text-left text-2xl text-white">Email:</span>
+          <input
+            className="px-2 py-1 w-36 sm:w-48 text-xl border-solid border-2 border-yellow-600 focus:border-yellow-700 focus:outline-none"
+            type="text"
+            name="name"
+            placeholder="Enter email"
+          />
+        </label>
+        <label className="flex items-center mb-4">
+          <span className="w-32 text-left text-2xl text-white">Username:</span>
+          <input
+            className="px-2 py-1 w-36 sm:w-48 text-xl border-solid border-2 border-yellow-600 focus:border-yellow-700 focus:outline-none"
+            type="text"
+            name="name"
+            placeholder="Enter username"
+          />
+        </label>
+        <label className="flex items-center">
+          <span className="w-32 text-left text-2xl text-white">Password:</span>
+          <input
+            type="password"
+            className="px-2 py-1 w-36 sm:w-48 text-xl border-solid border-2 border-yellow-600 focus:border-yellow-700 focus:outline-none"
+            placeholder="********"
+          />
+        </label>
+        <button className="px-4 py-1 mt-6 text-xl text-yellow-700 bg-white rounded-md border-solid border-4 border-yellow-700 transform transition-transform duration-500 hover:scale-110 relative group">
+          Sign Up
+          <span className="absolute bottom-[-0.5rem] left-2 w-full h-2 transform scale-y-0 bg-yellow-700 transition-transform duration-500 group-hover:scale-y-100 rounded-l-sm"></span>
+          <span className="absolute top-2 right-[-0.5rem] h-full w-2 transform scale-x-0 bg-yellow-700 transition-transform duration-500 group-hover:scale-x-100 rounded-r-sm"></span>
+        </button>
+        <p className="mt-3 text-xl text-white inline">
+          Already have an acccount?{" "}
+          <Link href="/sign-in">
+            <strong>Sign in</strong>
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
+}
