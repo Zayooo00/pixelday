608e20cdc6366f831bbdbf7dbb3791b1cab8a188
Zayooo00 <mrcold135@gmail.com>
2023-11-07 21:16:16 +0100

    created date display component

---
+++ components/dateDisplay.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export default function DateDisplay() {
+  const [formattedDate, setFormattedDate] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let date = new Date();
+      let day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+      let month = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+      let year = date.getFullYear();
+      let hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
+      let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+      setFormattedDate(`${year}-${month}-${day} ${hours}:${minutes}`);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <h1 className="text-4xl font-bold text-white text-center text-stroke-rose">{formattedDate}</h1>;
+}
