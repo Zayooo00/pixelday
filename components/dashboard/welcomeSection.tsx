f442b47710aa74b7f40815265c7c68f735b0c9f3
Zayooo00 <mrcold135@gmail.com>
2023-11-08 22:42:10 +0100

    implemented user name fetch and displau

--- components/dashboard/welcomeSection.tsx
+++ components/dashboard/welcomeSection.tsx
@@ -1,18 +1,39 @@
-"use client"
+"use client";
 
 import { GiJusticeStar } from "react-icons/gi";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { BiLogOut } from "react-icons/bi";
 
-import DateDisplay from "../dateDisplay";
+import DateDisplay from "@/components/dateDisplay";
 
-export default function WelcomeSection() {
-  const [user] = useState("Test123");
+import { auth } from "@/firebase/firebase";
+import { UserInfo } from "@/models/user-schema";
+
+
+export default function WelcomeSection({ currentUser }: { currentUser: UserInfo }) {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      router.push("/");
+    } catch (error) {}
+  };
+
   return (
     <>
-      <div className="flex items-center border-x-[10px] border-t-[10px] p-2 border-red-900 bg-amber-50">
+      <div className="flex items-center justify-between border-x-[10px] border-t-[10px] p-2 border-red-900 bg-amber-50">
         <GiJusticeStar className="text-red-900 text-3xl mr-2 border-2 border-red-900 bg-white" />
-        <h1 className="text-xl text-red-900 w-full pl-2 border-2 border-red-900 bg-white">
-          Welcome again, <strong>{user}</strong>
+        <h1 className="flex justify-between items-center text-xl text-red-900 w-full pl-2 border-2 border-red-900 bg-white">
+          <span>
+          Welcome again, <strong>{currentUser ? currentUser.displayName : 'Guest'}</strong>
+          </span>
+          <button
+            className="mr-1 p-0.5 hover:bg-rose-200 transition-colors duration-600 rounded-full"
+            onClick={handleLogout}
+          >
+            <BiLogOut className="text-red-700" />
+          </button>
         </h1>
       </div>
       <div className="h-2/6 sm:h-1/6 p-4 border-[10px] border-red-900 bg-amber-50">
