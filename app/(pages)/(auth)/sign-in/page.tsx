31d167d6a1c1245c8549ac4c59e6f71607bb27dd
Zayooo <mrcold135@gmail.com>
2023-11-08 11:26:18 +0100

    Implement google sign in functionality #16 (#25)
    * changed spinner theme
    
    * implemented google sign in

--- app/(pages)/(auth)/sign-in/page.tsx
+++ app/(pages)/(auth)/sign-in/page.tsx
@@ -3,16 +3,18 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { FirebaseError } from "firebase/app";
+import { FaGoogle } from "react-icons/fa";
 
-import { auth } from "@/firebase/firebase";
+import { auth, googleProvider } from "@/firebase/firebase";
 import {
   ERROR_INVALID_EMAIL,
   ERROR_WRONG_PASSWORD,
   ERROR_USER_NOT_FOUND,
   ERROR_MISSING_PASSWORD,
   ERROR_NETWORK_REQUEST_FAILED,
+  ERROR_POPUP_CLOSED,
 } from "@/firebase/firebaseErrorCodes";
 import Spinner from "@/components/spinner";
 
@@ -20,6 +22,7 @@ export default function SignIn() {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
   const [isAuthLoading, setIsAuthLoading] = useState(false);
+  const [isGoogleAuthLoading, setIsGoogleAuthLoading] = useState(false);
   const router = useRouter();
 
   const handleSignIn = async (e: React.FormEvent) => {
@@ -46,6 +49,21 @@ export default function SignIn() {
       }
     } finally {
       setIsAuthLoading(false);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    setIsGoogleAuthLoading(true);
+    try {
+      await signInWithPopup(auth, googleProvider);
+      router.push("/dashboard");
+    } catch (error) {
+      const { code, message } = error as FirebaseError;
+      if (code === ERROR_POPUP_CLOSED) {
+        setError("Popup has been closed by the user");
+      } 
+    } finally {
+      setIsGoogleAuthLoading(false);
     }
   };
 
@@ -89,14 +107,35 @@ export default function SignIn() {
             placeholder="********"
           />
         </label>
+        <div className="flex items-center justify-center gap-4">
         <button
-          className="px-4 py-1 mt-6 text-xl text-red-700 bg-white rounded-md border-solid border-4 border-red-700 transform transition-transform duration-500 hover:scale-110 relative group"
+          disabled={!credentials.email || !credentials.password}
+          className={`px-4 py-1 mt-6 text-xl text-red-700 bg-white rounded-md border-solid border-4 border-red-700 transform transition-transform duration-500 hover:scale-110 relative group ${!credentials.email || !credentials.password ? 'cursor-not-allowed' : ''}`}
           onClick={handleSignIn}
         >
-          {isAuthLoading ? <Spinner/> : "Sign In"}
-          <span className="absolute bottom-[-0.5rem] left-2 w-full h-2 transform scale-y-0 bg-red-700 transition-transform duration-500 group-hover:scale-y-100 rounded-l-sm"/>
-          <span className="absolute top-2 right-[-0.5rem] h-full w-2 transform scale-x-0 bg-red-700 transition-transform duration-500 group-hover:scale-x-100 rounded-r-sm"/>
+          {isAuthLoading ? <Spinner /> : "Sign In"}
+          <span className="absolute bottom-[-0.5rem] left-2 w-full h-2 transform scale-y-0 bg-red-700 transition-transform duration-500 group-hover:scale-y-100 rounded-l-sm" />
+          <span className="absolute top-2 right-[-0.5rem] h-full w-2 transform scale-x-0 bg-red-700 transition-transform duration-500 group-hover:scale-x-100 rounded-r-sm" />
         </button>
+        <button
+          className="px-4 py-1 mt-6 text-xl text-red-700 bg-white rounded-md border-solid border-4 border-red-700 transform transition-transform duration-500 hover:scale-110 relative group flex items-center"
+          onClick={handleGoogleSignIn}
+        >
+          {isGoogleAuthLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              <FaGoogle
+                alt="Google sign in button"
+                className="text-red-700 mr-1"
+              />{" "}
+              <span>Google</span>
+              <span className="absolute bottom-[-0.5rem] left-2 w-full h-2 transform scale-y-0 bg-red-700 transition-transform duration-500 group-hover:scale-y-100 rounded-l-sm" />
+          <span className="absolute top-2 right-[-0.5rem] h-full w-2 transform scale-x-0 bg-red-700 transition-transform duration-500 group-hover:scale-x-100 rounded-r-sm" />
+            </>
+          )}
+        </button>
+        </div>
         <p className="mt-3 text-xl text-white inline">
           Don&apos;t have an account?{" "}
           <Link href="/sign-up">
