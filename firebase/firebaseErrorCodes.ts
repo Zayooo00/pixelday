5f7f290d845fbea42957ebc0fefd9a9efe6d2d91
Zayooo00 <mrcold135@gmail.com>
2023-12-15 17:39:06 +0100

    added firebase sign up error

--- firebase/firebaseErrorCodes.ts
+++ firebase/firebaseErrorCodes.ts
@@ -4,3 +4,4 @@ export const ERROR_USER_NOT_FOUND = 'auth/user-not-found';
 export const ERROR_MISSING_PASSWORD = 'auth/missing-password';
 export const ERROR_POPUP_CLOSED = 'auth/popup-closed-by-user';
 export const ERROR_NETWORK_REQUEST_FAILED = 'network-request-failed';
+export const ERROR_EMAIL_ALREADY_IN_USE = 'auth/email-already-in-use';
